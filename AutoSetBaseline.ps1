9e371dbdf61ccf825c2b4875d87fcb3d044f9286
mchangchien <67855568+mchangchien@users.noreply.github.com>
2022-08-25 15:07:32 +0800

    fix indent

--- AutoSetBaseline.ps1
+++ AutoSetBaseline.ps1
@@ -21,7 +21,7 @@ mchangchien0501 = @{'ServerName' = 'mchangchien0501'; 'ServerInstance' =  'mchan
 $baseline_query = "SELECT USER_NAME(member_principal_id) AS [Owner] FROM sys.database_role_members WHERE USER_NAME(role_principal_id) = 'db_owner' AND USER_NAME(member_principal_id) != 'dbo'"
 
 $params = @{'Database' = 'Database1'; 'ServerInstance' =  'mchangchien0705.database.windows.net'; 'Username' = 'azureuser'; 'Password' = 'Azure@User123'; 'OutputSqlErrors' = $true;
-		'Query' = "SELECT USER_NAME(member_principal_id) AS [Owner] FROM sys.database_role_members WHERE USER_NAME(role_principal_id) = 'db_owner' AND USER_NAME(member_principal_id) != 'dbo'" }
+    'Query' = "SELECT USER_NAME(member_principal_id) AS [Owner] FROM sys.database_role_members WHERE USER_NAME(role_principal_id) = 'db_owner' AND USER_NAME(member_principal_id) != 'dbo'" }
 
 $rule_id = 'VA1258'
 $outputFilePath = "C:\Users\mchangchien\Desktop\output.txt"
@@ -38,73 +38,73 @@ $outputFilePath = "C:\Users\mchangchien\Desktop\output.txt"
 $server_result = Get-AzResourceGroup | Get-AzSqlServer
 For ($i = 0; $i -lt $server_result.count; $i++)
 {
-	Write-host "----------------------------"; 
+  Write-host "----------------------------"; 
   Add-Content -Path $outputFilePath -Value "----------------------------"
   Write-host $server_result[$i].servername; 
   Add-Content -Path $outputFilePath -Value $server_result[$i].servername;
-	#Write-host $server_result[$i].resourcegroupname; 
-	#Write-host $server_result[$i].fullyqualifieddomainname; 
-	#Write-host "----------------------------";
-	
-	$server_name = $server_result[$i].servername
-	$rg_name = $server_result[$i].resourcegroupname; 
-	$server_fqdn = $server_result[$i].fullyqualifieddomainname;
-	
-	
-	if ($ServerAccountPwd.$server_name -eq $null)
-	{
-		Write-host "no password information, skip the server:$server_name";
+  #Write-host $server_result[$i].resourcegroupname; 
+  #Write-host $server_result[$i].fullyqualifieddomainname; 
+  #Write-host "----------------------------";
+  
+  $server_name = $server_result[$i].servername
+  $rg_name = $server_result[$i].resourcegroupname; 
+  $server_fqdn = $server_result[$i].fullyqualifieddomainname;
+  
+  
+  if ($ServerAccountPwd.$server_name -eq $null)
+  {
+    Write-host "no password information, skip the server:$server_name";
     Add-Content -Path $outputFilePath -Value "no password information, skip the server:$server_name";
     Write-host "----------------------------";
     Add-Content -Path $outputFilePath -Value "----------------------------";
-		continue
-	}
-	else
-	{
-		Write-host "has account and password information";
+    continue
+  }
+  else
+  {
+    Write-host "has account and password information";
     Add-Content -Path $outputFilePath -Value "has account and password information";
-	}
-	
+  }
+  
   # store the server account and password info
-	$server_admin = $ServerAccountPwd.$server_name.Username
-	$server_admin_pwd = $ServerAccountPwd.$server_name.Password
-	
+  $server_admin = $ServerAccountPwd.$server_name.Username
+  $server_admin_pwd = $ServerAccountPwd.$server_name.Password
+  
   # get a list of databases other than master db in the server
-	$database_list = Get-AzSqlDatabase -ServerName $server_name -ResourceGroupName $rg_name
-	
+  $database_list = Get-AzSqlDatabase -ServerName $server_name -ResourceGroupName $rg_name
+  
   # for each database other than master, we run the VA query and set the result as new baseline
-	For ($j = 0; $j -lt $database_list.count; $j++) 
-	{
-		$database_name = $database_list[$j].databasename
-		if($database_name -eq 'master')
-		{continue}
-		Write-host $database_name;
+  For ($j = 0; $j -lt $database_list.count; $j++) 
+  {
+    $database_name = $database_list[$j].databasename
+    if($database_name -eq 'master')
+    {continue}
+    Write-host $database_name;
     Add-Content -Path $outputFilePath -Value "Database name: $database_name";
-		
+    
     # prepare the parameters for executing query
-		$params = @{'Database' = $database_name; 'ServerInstance' =  $server_fqdn; 'Username' = $server_admin; 'Password' = $server_admin_pwd; 'OutputSqlErrors' = $true;
-		'Query' = $baseline_query }
-		
+    $params = @{'Database' = $database_name; 'ServerInstance' =  $server_fqdn; 'Username' = $server_admin; 'Password' = $server_admin_pwd; 'OutputSqlErrors' = $true;
+    'Query' = $baseline_query }
+    
     # execute query and store result
-		$query_result = Invoke-Sqlcmd  @params
-		Start-Sleep -s 2
-		Write-host $query_result.count
-		
+    $query_result = Invoke-Sqlcmd  @params
+    Start-Sleep -s 2
+    Write-host $query_result.count
+    
     # skip those datanases that have no result, meaning these databases have no need to set the baseline.
-		if( ($query_result.count -gt 0)) # -and ($database_name -eq 'Database1') )
-		{
-			$arrayList = [System.Collections.ArrayList]::new()
-			For ($k = 0; $k -lt $query_result.count; $k++)
-			{
-				Write-host $query_result[$k].Owner;
-				$arrayList.Add(@($query_result[$k].Owner))			
-			}
-			#Write-host $query_result
-      Add-Content -Path $outputFilePath -Value $arrayList	;
-			#Write-host $arrayList	
-			Set-AzSqlDatabaseVulnerabilityAssessmentRuleBaseline -ResourceGroupName $rg_name -ServerName $server_name -DatabaseName $database_name -BaselineResult $arrayList -RuleID $rule_id | Out-File -FilePath $outputFilePath -Append
-		}
-	}
+    if( ($query_result.count -gt 0)) # -and ($database_name -eq 'Database1') )
+    {
+      $arrayList = [System.Collections.ArrayList]::new()
+      For ($k = 0; $k -lt $query_result.count; $k++)
+      {
+        Write-host $query_result[$k].Owner;
+        $arrayList.Add(@($query_result[$k].Owner))      
+      }
+      #Write-host $query_result
+      Add-Content -Path $outputFilePath -Value $arrayList  ;
+      #Write-host $arrayList  
+      Set-AzSqlDatabaseVulnerabilityAssessmentRuleBaseline -ResourceGroupName $rg_name -ServerName $server_name -DatabaseName $database_name -BaselineResult $arrayList -RuleID $rule_id | Out-File -FilePath $outputFilePath -Append
+    }
+  }
   #Write-host "----------------------------";
-	
+  
 }
